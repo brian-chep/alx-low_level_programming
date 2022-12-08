910b5aaa400e6599990d978e35a09cf12885e8fc
Brian Chepchieng <chepchiengbrian@gmail.com>
2022-12-08 06:58:36 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,13 @@
 
 
 /**
- * Entry point
+ * main - Entry point
  * 
- * This is the main function
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle,");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
