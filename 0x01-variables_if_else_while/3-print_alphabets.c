50290c72656a5703ea5b4a9f96fa8f99f4f51a9b
Brian Chepchieng <chepchiengbrian@gmail.com>
2022-12-09 16:29:09 -0800

    alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int alpha;
+	int beta;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	for (beta = 'A'; beta <= 'Z'; beta++)
+		putchar(beta);
+	putchar(\n);
+	return (0);
+}
