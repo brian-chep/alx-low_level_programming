ce751291c0a4577bf56d8af528719cb90fc8e0c9
Brian Chepchieng <chepchiengbrian@gmail.com>
2022-12-14 07:47:10 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,7 @@
-#include "main.h"
-
-/**
- * main - Entry Point
- *
- * Return (0) Success
- *
- */
-
 void print_alphabet(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; ++c)
-		_putchar("%c \n", c);
+		printf("%c \n", c);
 }
