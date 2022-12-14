c82e749134a505c61f79bf8762a646c68aad8c78
Brian Chepchieng <chepchiengbrian@gmail.com>
2022-12-14 09:05:50 -0800

    number 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - calculate the absoluten of a number
+ *
+ * to be calculated
+ *
+ * Return
+ *
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (-1 * n);
+	else
+		return (n);
+}
