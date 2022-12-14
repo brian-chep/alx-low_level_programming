4c3de662b7ffc6e2b2980e48eb528af710a62142
Brian Chepchieng <chepchiengbrian@gmail.com>
2022-12-14 08:41:55 -0800

    alphabet lower case

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,13 +6,12 @@
  * Description: print_alphabet prints alphabet in lowercase
  *
  * print alphabet in lowercase
- *
  */
 
 /* print_alphabet prints in lowercase */
 void print_alphabet(void)
-/* prints in lower case*/
 {
+/* prints in lower case*/
 	char c;
 
 	for (c = 'a'; c <= 'z'; ++c)
