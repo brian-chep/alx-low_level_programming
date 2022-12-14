78cf8850c9acb3a29a85f79e923d5b29f734331d
Brian Chepchieng <chepchiengbrian@gmail.com>
2022-12-14 08:54:48 -0800

    c is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Description: print_alphabet prints alphabet in lowercase
+ *
+ * print alphabet in lowercase
+ */
+
+int _islower(int c)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	printf("%c", c);
+	printf("\n");
+}
