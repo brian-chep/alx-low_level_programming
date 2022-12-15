f92eb137ec0d2c14ca4232ff74f6f3637152b69b
Brian Chepchieng <chepchiengbrian@gmail.com>
2022-12-14 18:10:13 -0800

    to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+
+/**
+ * print_to_98 - print all natural from 8 to 98
+ *
+ * @n: value to serve as range
+ *
+ * Return: Nothing
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n > 98; n--)
+			printf("%d, ", n);
+		printf("%d\n", 98);
+	}
+
+	else
+	{
+		for (; n < 98; n++)
+			printf("%d, ", n);
+		printf("%d\n", 98);
+	}
+}
