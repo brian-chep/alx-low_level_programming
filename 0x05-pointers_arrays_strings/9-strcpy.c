35b63cf8c2b0062a78d4ffebc26a36d76337d7d3
Brian Chepchieng <chepchiengbrian@gmail.com>
2022-12-19 11:48:17 -0800

    function copying data referenced

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
-
 /**
- * _strcpy -  copies the string pointed to by src including
+ * *_strcpy -  copies the string pointed to by src including
+ *
  * the terminating null byte (\0), to the buffer pointed to by dest
  *
  * @dest: buffer
- *
- * @src: source string to copy
  *
  * Return: the pointer to dest
  */
