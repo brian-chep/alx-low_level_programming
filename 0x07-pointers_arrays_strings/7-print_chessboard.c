634bb473ebc43c20fd76c152138b0c494161791b
Brian Chepchieng <chepchiengbrian@gmail.com>
2023-01-03 09:55:35 -0800

    prints the chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * _putchar - prints the chessboard to stdout
+ * @a: chessboard to be printed
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; a[i][7]; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+
+		_putchar('\n');
+	}
+}
