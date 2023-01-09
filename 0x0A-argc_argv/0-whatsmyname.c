0f6d6452933a786defa3ebc8bfc2da4ae2e87314
Brian Chepchieng <chepchiengbrian@gmail.com>
2023-01-09 08:48:34 -0800

    prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - print name of the program
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: Always 0
+ */
+ 
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+
