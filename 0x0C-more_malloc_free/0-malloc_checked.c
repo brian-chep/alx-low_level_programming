547071a9cdcde72f91dda7d1a0bd0290bca9d419
Brian Chepchieng <chepchiengbrian@gmail.com>
2023-01-13 18:50:24 -0800

    allocates pointer using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory
+ * @b: input value
+ *
+ * Return: memory pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	ptr = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
