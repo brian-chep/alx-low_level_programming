494b9b885d78ec769f94a762bfa4394d2519d7cd
Brian Chepchieng <chepchiengbrian@gmail.com>
2023-01-21 00:30:39 -0800

    performs simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+/**
+ * op_add - add
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulus
+ * @a: integer
+ * @b: integer
+ * Return: result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
