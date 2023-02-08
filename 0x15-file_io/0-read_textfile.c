207d5bd37b9ef1a9fa1d1448e996dde8cb67a184
Brian Chepchieng <chepchiengbrian@gmail.com>
2023-02-08 10:51:08 -0800

    POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,25 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-/* malloc, free */
-#include <stdlib.h>
-
-/* dprintf */
-#include <stdio.h>
-
-/* open */
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
-/*close */
-#include <unistd.h>
-
-ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
-
-#endif#include "main.h"
+#include "main.h"
 
 /**
  * read_textfile - reads a text file and prints it to POXIS stdout
